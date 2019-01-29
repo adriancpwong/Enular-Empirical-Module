3f5928f34ed4177f26c445ff54eb85b4fc85fac4
Adrian <modoseinal@hotmail.com>
2019-01-29 05:23:52 +0000

    Update test.py

--- test.py
+++ test.py
@@ -28,5 +28,9 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 import selenium
+from datetime import datetime
+import smtplib
+from selenium import webdriver
+
 
 print ("zac is a faggot")
