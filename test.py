8eb8b3257c85f3ae6ad62f9b48724efef39112b0
Adrian <modoseinal@hotmail.com>
2019-01-29 07:00:27 +0000

    checkpoint

--- test.py
+++ test.py
@@ -31,6 +31,84 @@ import selenium
 from datetime import datetime
 import smtplib
 from selenium import webdriver
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing
+
+def getStocks():
 
 
-print ("zac is a faggot")
+    stock_list = []
+    stock_list.append('AAPL')
+    stock_list.append('ADBE')
+    stock_list.append('AMZN')
+    
+    #Using the stock list to predict the future price of the stock a specificed amount of days
+    number = 0
+    for i in stock_list:
+        print("Number: " + str(number))
+        predictData(i,1)
+        number += 1
+
+def predictData(stock, days):
+    print(stock)
+
+    start = datetime(2017, 1, 1)
+    end = datetime.now()
+
+    #Outputting the Historical data into a .csv for later use
+    df = get_historical_data(stock, start=start, end=end, output_format='pandas')
+    
+    if os.path.exists('./Exports'):
+        csv_name = ('Exports/' + stock + '_Export.csv')
+    else:
+        os.mkdir("Exports")
+        csv_name = ('Exports/' + stock + '_Export.csv')
+    df.to_csv(csv_name)
+
+    Xp = np.array(df)[-1]
+    print(Xp)
+
+    df['prediction'] = df['close'].shift(-1)
+    df.dropna(inplace=True)
+  
+    
+    forecast_time = int(days)
+
+    #Predicting the Stock price in the future
+    X = np.array(df.drop(['prediction'], 1))
+    print(X[-1])
+    print(X.shape)
+
+    X = np.vstack([X, Xp])
+    print(X[-1])
+    print(X.shape)
+
+    Y = np.array(df['prediction'])
+    Xs = preprocessing.scale(X)
+
+    Xp = Xs[-1]
+    Xs = Xs[:-1, :]
+    print(Xs.shape)
+    print(Xs[-1])
+    print(Xp)
+    
+    X_prediction = np.reshape(Xp,(1, 5))
+    X_train, X_test, Y_train, Y_test = train_test_split(Xs, Y, test_size=0.2)
+
+    #Performing the Regression on the training data
+    clf = LinearRegression()
+    clfcv = LinearRegression()
+
+    clf.fit(Xs, Y)
+    prediction = (clf.predict(X_prediction))
+
+    clfcv.fit(X_train,Y_train)
+    print(clfcv.score(X_test,Y_test))
+
+    #last_row = df.tail(1)
+    
+    #print(last_row['close'])
+    print(prediction)
+
+if __name__ == '__main__':
+    getStocks()
